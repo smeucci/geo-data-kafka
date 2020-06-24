4999df0be2565a728c353ca7ff23082ab63ed878
Saverio <s.meucci91@gmail.com>
2020-06-24 11:53:02 +0200

    GeoData class to represent the record for the geo.data topic

---
+++ src/main/java/com/github/smeucci/geo/data/kafka/record/GeoData.java
@@ -0,0 +1,19 @@
+package com.github.smeucci.geo.data.kafka.record;
+
+import java.time.Instant;
+
+public record GeoData(long timestamp, double latitude, double longitude) {
+
+	public static GeoData generate() {
+
+		long timestamp = Instant.now().toEpochMilli();
+
+		double latitude = (Math.random() * 180) - 90;
+
+		double longitude = (Math.random() * 360) - 180;
+
+		return new GeoData(timestamp, latitude, longitude);
+
+	}
+
+}
