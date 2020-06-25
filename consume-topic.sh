16fd0a3e3c2509eb34f92b82e03ef3404b62ca40
Saverio <s.meucci91@gmail.com>
2020-06-25 12:03:16 +0200

    sh script to consume topics

---
+++ consume-topic.sh
@@ -0,0 +1,18 @@
+echo $1 $2
+
+topic=$1
+value=${2-"string"}
+
+if [ $value == "long" ]
+then
+
+	kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic $topic \
+		--property print.key=true \
+		--property print.value=true \
+		--property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
+
+else
+	
+	kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic $topic
+	
+fi
