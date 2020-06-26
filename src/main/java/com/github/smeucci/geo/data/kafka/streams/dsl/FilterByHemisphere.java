c9262bfc14026560d696496761b13bcc5464ce0f
Saverio <s.meucci91@gmail.com>
2020-06-26 15:26:09 +0200

    compact processors

--- src/main/java/com/github/smeucci/geo/data/kafka/streams/dsl/FilterByHemisphere.java
+++ src/main/java/com/github/smeucci/geo/data/kafka/streams/dsl/FilterByHemisphere.java
@@ -15,30 +15,28 @@ public class FilterByHemisphere {
 	public static void northern(final KStream<String, String> geoDataStream) {
 
 		// filter for northern hemisphere geo data
-		KStream<String, String> northernHemisphereStream = geoDataStream
+		geoDataStream
 				// keep northern hemisphere geo data
 				.filter(GeoDataUtils.isInNorthernHemisphere,
 						Named.as(GeoDataConfig.Processor.FILTER_NORTHERN.processorName()))
 				// peek geo data
-				.peek((k, v) -> log.info("Northern Hemisphere: {}", v));
-
-		// set output topic
-		northernHemisphereStream.to(GeoDataConfig.Topic.NORTHERN_HEMISPHERE_GEO_DATA.topicName());
+				.peek((k, v) -> log.info("Northern Hemisphere: {}", v))
+				// set output topic
+				.to(GeoDataConfig.Topic.NORTHERN_HEMISPHERE_GEO_DATA.topicName());
 
 	}
 
 	public static void southern(final KStream<String, String> geoDataStream) {
 
 		// filter for northern hemisphere geo data
-		KStream<String, String> southernHemisphereStream = geoDataStream
+		geoDataStream
 				// keep northern hemisphere geo data
 				.filter(GeoDataUtils.isInSouthernHemisphere,
 						Named.as(GeoDataConfig.Processor.FILTER_SOUTHERN.processorName()))
 				// peek geo data
-				.peek((k, v) -> log.info("Southern Hemisphere: {}", v));
-
-		// set output topic
-		southernHemisphereStream.to(GeoDataConfig.Topic.SOUTHERN_HEMISPHERE_GEO_DATA.topicName());
+				.peek((k, v) -> log.info("Southern Hemisphere: {}", v))
+				// set output topic
+				.to(GeoDataConfig.Topic.SOUTHERN_HEMISPHERE_GEO_DATA.topicName());
 
 	}
 
